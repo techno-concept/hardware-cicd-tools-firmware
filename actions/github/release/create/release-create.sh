012804bf98aa50a42b1e1da4453d6c0c366e1164
Sebastien PULVERAIL <s.pulverail@technoconcept.com>
2024-01-08 14:36:11 +0100

    Add a composite action to create a release

---
+++ actions/github/release/create/release-create.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Parameters
+DRAFT=$1
+PRERELEASE=$2
+
+# Convert the string input to a proper boolean
+if [ "$DRAFT" == "true" ]; then
+  DRAFT_BOOL=true
+else
+  DRAFT_BOOL=false
+fi
+if [ "$PRERELEASE" == "true" ]; then
+  PRERELEASE_BOOL=true
+else
+  PRERELEASE_BOOL=false
+fi
+
+# Set the GitHub API URL
+API_URL="$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases"
+
+# Make the API call with curl
+RESPONSE=$(curl -s -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Content-Type: application/json" \
+  $API_URL \
+  -d '{
+        "draft": '$DRAFT_BOOL',
+        "generate_release_notes": 'false',
+        "name": "'$RELEASE_TAG'",
+        "tag_name": "'$RELEASE_TAG'",
+        "draft": '$DRAFT_BOOL'
+      }')
+
+echo "RESPONSE: $RESPONSE"
+
+# Check for a successful response and parse needed data
+if [ "$(echo "$RESPONSE" | jq -r '.id')" != null ]; then
+  # Parse variables
+  RELEASE_ID=$(echo "$RESPONSE" | jq -r '.id')
+  RELEASE_HTML_URL=$(echo "$RESPONSE" | jq -r '.html_url')
+  RELEASE_UPLOAD_URL=$(echo "$RESPONSE" | jq -r '.upload_url')
+
+  # Set outputs for GitHub Actions
+  echo "RELEASE_ID=$RELEASE_ID" >> $GITHUB_ENV
+  echo "RELEASE_HTML_URL=$RELEASE_HTML_URL" >> $GITHUB_ENV
+  echo "RELEASE_UPLOAD_URL=$RELEASE_UPLOAD_URL" >> $GITHUB_ENV
+
+  echo "Release created successfully: ID $RELEASE_ID, URL $RELEASE_HTML_URL, UPLOAD URL $RELEASE_UPLOAD_URL"
+else
+  echo "Failed to create release"
+  echo "Response: $RESPONSE"
+  exit 1
+fi
